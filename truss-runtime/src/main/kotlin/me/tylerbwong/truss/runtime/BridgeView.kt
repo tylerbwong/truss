420f0027aa3051d038e4471bc21d0df442a39ab1
Tyler Wong <tbwong3@gmail.com>
2021-06-05 01:03:29 -0700

    Remove extra annotation

--- truss-runtime/src/main/kotlin/me/tylerbwong/truss/runtime/BridgeView.kt
+++ truss-runtime/src/main/kotlin/me/tylerbwong/truss/runtime/BridgeView.kt
@@ -1,12 +1,10 @@
 package me.tylerbwong.truss.runtime
 
+/**
+ * Applying this annotation will generate an associated Android View to be used as a bridge to
+ * Composable functions.
+ */
 @MustBeDocumented
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.FUNCTION)
 annotation class BridgeView
-
-@MustBeDocumented
-@Retention(AnnotationRetention.SOURCE)
-@Target(AnnotationTarget.FUNCTION)
-annotation class Composable
-
